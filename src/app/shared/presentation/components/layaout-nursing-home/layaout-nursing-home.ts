976350812118f21ecc6aa744a5139690fe61f271
RenatoCY <renato.calvo2@hotmail.com>
2025-09-29 15:54:21 -0500

    feat: add language switcher, layout nursing home, and toolbar components

---
+++ src/app/shared/presentation/components/layaout-nursing-home/layaout-nursing-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layaout-nursing-home',
+  imports: [],
+  templateUrl: './layaout-nursing-home.html',
+  styleUrl: './layaout-nursing-home.css'
+})
+export class LayaoutNursingHome {
+
+}
