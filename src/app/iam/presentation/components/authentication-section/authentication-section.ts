40148a49b6e1bee371385f370237b719ca65bfb6
RenatoCY <renato.calvo2@hotmail.com>
2025-10-04 07:40:14 -0500

    refactor: update  authentication-section

--- src/app/iam/presentation/components/authentication-section/authentication-section.ts
+++ src/app/iam/presentation/components/authentication-section/authentication-section.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButton} from '@angular/material/button';
-/**import { AuthenticationService } from '../../services/authentication.service';*/
 @Component({
   selector: 'app-authentication-section',
   standalone: true,
@@ -14,29 +13,17 @@ import {MatButton} from '@angular/material/button';
   styleUrl: './authentication-section.css'
 })
 export class AuthenticationSection {
-  currentEmail: string = '';
-  isSignedIn: boolean = false;
   constructor(
     private router: Router,
-    /*private authenticationService: AuthenticationService*/
   ) {
-    /*this.authenticationService.currentEmail.subscribe(
-      (email:string) => this.currentEmail = email
-    );*/
-    /*this.authenticationService.isSignedIn.subscribe(
-      (isSignedIn:boolean) => this.isSignedIn = isSignedIn
-    );*/
+
   }
-  onSignIn() {
-    // Navigate to the sign-in page.
-    this.router.navigate(['/sign-in']).then();
-  }
+
   onRegisterAdmin() {
-    // Navigate to the admin registration page.
-    this.router.navigate(['/register-administrator']).then();
+    void this.router.navigate(['auth/sign-up/administrator']);
   }
+
   onRegisterFamily() {
-    // Navigate to the family registration page.
-    this.router.navigate(['/register-family-member']).then();
+    void this.router.navigate(['auth/sign-up/familiar']);
   }
 }
