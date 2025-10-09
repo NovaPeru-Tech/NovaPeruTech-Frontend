71bdb4dc566db52a11a07e923575466b5012b400
RenatoCY <renato.calvo2@hotmail.com>
2025-10-09 18:55:15 -0500

    feat(router): add router and environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,7 @@ export const environment = {
   platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
   platformProviderSignInEndpointPath:'/users',
   platformProviderSignUpEndpointPath:'/users',
+  platformProviderRegisterNursingHomeEndPoint:'/nursingHome',
+  platformProviderResidentPath:'/residents'
+
 };
