78d8167dda024fe1e9035bef90075c4b3167d5bc
Billy Jake Ruiz Madrid <u202116401@upc.edu.pe>
2025-10-10 00:17:58 -0500

    feat(nursing-home): add nursing home entity.

---
+++ src/app/nursingHome/domain/model/nursing-home.entity.ts
@@ -0,0 +1,76 @@
+export class NursingHome
+{
+  private _id:number;
+  private _name:string;
+  private _ruc:string;
+  private _phoneNumber:string;
+  private _address:string;
+  private _description:string;
+  private _AdminId:number;
+  constructor(nursingHome:{id:number,name:string,ruc:string,phoneNumber:string,address:string,description:string,adminId:number}) {
+    this._id = nursingHome.id;
+    this._name = nursingHome.name;
+    this._phoneNumber = nursingHome.phoneNumber;
+    this._address = nursingHome.address;
+    this._AdminId = nursingHome.adminId;
+    this._ruc = nursingHome.ruc;
+    this._description = nursingHome.description;
+
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get ruc(): string {
+    return this._ruc;
+  }
+
+  set ruc(value: string) {
+    this._ruc = value;
+  }
+
+  get phoneNumber(): string {
+    return this._phoneNumber;
+  }
+
+  set phoneNumber(value: string) {
+    this._phoneNumber = value;
+  }
+
+  get address(): string {
+    return this._address;
+  }
+
+  set address(value: string) {
+    this._address = value;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  set description(value: string) {
+    this._description = value;
+  }
+
+  get adminId(): number {
+    return this._AdminId;
+  }
+
+  set adminId(value: number) {
+    this._AdminId = value;
+  }
+}
