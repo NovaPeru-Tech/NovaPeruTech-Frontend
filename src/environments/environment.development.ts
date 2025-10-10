5f14512de2670ddba7c0585ee3a6bab1f0a5ed1d
Racso24k <u20211g192@upc.edu.pe>
2025-10-10 16:42:48 -0500

    feat(env): update API base URL for development and production environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: false,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
-  platformProviderSignInEndpointPath:'/users',
-  platformProviderSignUpEndpointPath:'/users',
-  platformProviderRegisterNursingHomeEndPoint:'/nursingHome',
-  platformProviderResidentPath:'/residents',
-  platformProviderEmployeeEndPoint:'/employee'
-
+  platformProviderApiBaseUrl: 'https://my-fake-api-open.vercel.app',
+  platformProviderSignInEndpointPath: '/users',
+  platformProviderSignUpEndpointPath: '/users',
+  platformProviderRegisterNursingHomeEndPoint: '/nursingHome',
+  platformProviderResidentPath: '/residents',
+  platformProviderEmployeeEndPoint: '/employee'
 };
