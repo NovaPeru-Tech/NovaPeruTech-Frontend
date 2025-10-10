2091d23898e92fc98823c52cecaf5ea5ea346aa5
Billy Jake Ruiz Madrid <u202116401@upc.edu.pe>
2025-10-10 00:35:42 -0500

    feat(routing): integrate nursing home module route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import {Home} from './shared/presentation/views/home/home';
 const pageNotFound=()=>import('./shared/presentation/views/page-not-found/page-not-found').then(m=>m.PageNotFound);
 const iamRoutes=()=>import('./iam/presentation/iam-routers').then(m=>m.IamRouters);
+const nursingHomeRoutes=()=>import('./nursingHome/presentation/nursing-home-routers').then(m=>m.NursingHomeRouters);
 const baseTitle='Veyra';
 export const routes: Routes = [
 
   {path:'home',        component:Home                ,title:`Home-${baseTitle}`},
   {path:'',            redirectTo:'/home'            ,pathMatch:'full' },
   {path:'auth', loadChildren:iamRoutes },
+  { path:'', loadChildren:nursingHomeRoutes},
   {path:'**',          loadComponent:pageNotFound    ,title:`Page Not Found-${baseTitle}`},
 
 ];
