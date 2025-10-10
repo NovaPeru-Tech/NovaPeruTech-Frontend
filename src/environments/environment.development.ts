59333c8886398cdf7de1b2721bb8e8d992514af5
Racso24k <u20211g192@upc.edu.pe>
2025-10-10 18:07:31 -0500

    feat(env): update platform provider API base URL to production endpoint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderApiBaseUrl: 'https://my-fake-api-open.vercel.app',
   platformProviderSignInEndpointPath:'/users',
   platformProviderSignUpEndpointPath:'/users',
   platformProviderRegisterNursingHomeEndPoint:'/nursingHome',
