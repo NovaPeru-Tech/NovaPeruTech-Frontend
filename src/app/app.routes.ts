771f86d65e62ce2842af73137bfd93ef3d70dbd7
Adrian QC <aqc1019@gmail.com>
2025-10-24 20:38:47 -0500

    refactor(app-routes): improve readability of application routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,27 @@
 import { Routes } from '@angular/router';
-import {Home} from './shared/presentation/views/home/home';
-const pageNotFound=()=>import('./shared/presentation/views/page-not-found/page-not-found').then(m=>m.PageNotFound);
-const iamRoutes=()=>import('./iam/presentation/iam-routers').then(m=>m.IamRouters);
-const nursingHomeRoutes=()=>import('./nursingHome/presentation/nursing-home-routers').then(m=>m.NursingHomeRouters);
-const residentRoutes=()=>import('./resident/presentation/resident-router').then(m=>m.ResidentRouter);
-const employeeRoutes=()=>import('./employees/presentation/employee-routers').then(m=>m.EmployeeRouters);
-const inventoryRoutes=()=>import('./inventory/presentation/Inventory-router').then(m=>m.InventoryRouter);
-const baseTitle='Veyra';
+import { Home } from './shared/presentation/views/home/home';
+
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
+const iamRoutes = () =>
+  import('./iam/presentation/iam-routers').then(m => m.IamRouters);
+const nursingHomeRoutes = () =>
+  import('./nursingHome/presentation/nursing-home-routers').then(m => m.NursingHomeRouters);
+const residentRoutes = () =>
+  import('./resident/presentation/resident-router').then(m => m.ResidentRouter);
+const employeeRoutes = () =>
+  import('./employees/presentation/employee-routers').then(m => m.EmployeeRouters);
+const inventoryRoutes = () =>
+  import('./inventory/presentation/Inventory-router').then(m => m.InventoryRouter);
+
+const baseTitle = 'Veyra';
 export const routes: Routes = [
-
-  {path:'home',        component:Home                ,title:`Home-${baseTitle}`},
-  {path:'',            redirectTo:'/home'            ,pathMatch:'full' },
-  {path:'auth', loadChildren:iamRoutes },
-  { path:'', loadChildren:nursingHomeRoutes },
-  {path:'',loadChildren:residentRoutes},
-  {path:'',loadChildren:employeeRoutes},
-  { path:'inventory',loadChildren:inventoryRoutes},
-  {path:'**',          loadComponent:pageNotFound    ,title:`Page Not Found-${baseTitle}`},
+  { path: 'home',      component: Home,             title:`Home | ${baseTitle}` },
+  { path: 'auth',      loadChildren: iamRoutes },
+  { path: '',          loadChildren: nursingHomeRoutes },
+  { path: '',          loadChildren: residentRoutes },
+  { path: '',          loadChildren: employeeRoutes },
+  { path: 'inventory', loadChildren: inventoryRoutes },
+  { path: '',          redirectTo: '/home',         pathMatch:'full' },
+  { path: '**',        loadComponent: pageNotFound, title:`Page Not Found | ${baseTitle}`}
 ];
-
