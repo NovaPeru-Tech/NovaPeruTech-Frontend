dd24ef43a5c36121f0aa063468911c27ce6abb7e
Adrian QC <aqc1019@gmail.com>
2025-10-27 03:12:40 -0500

    feat(residents): add search functionality by first or last name in resident-list.

--- src/app/resident/presentation/resident-form-list/resident-form-list.ts
+++ src/app/resident/presentation/resident-form-list/resident-form-list.ts
@@ -1,13 +1,17 @@
-import { Component, computed, inject } from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import { ResidentStore } from '../../application/resident-store';
 import { Router } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { MatError } from '@angular/material/form-field';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { LayoutNursingHome } from '../../../shared/presentation/components/layout-nursing-home/layout-nursing-home';
+import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule}  from '@angular/forms';
 @Component({
   selector: 'app-resident-form-list',
   standalone: true,
@@ -19,8 +23,14 @@ import { LayoutNursingHome } from '../../../shared/presentation/components/layou
     MatCardContent,
     MatIcon,
     MatButton,
-    LayoutNursingHome
-
+    LayoutNursingHome,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormsModule,
+    MatIconButton,
+    MatInputModule,
+    MatFormFieldModule
   ],
   templateUrl: './resident-form-list.html',
   styleUrl: './resident-form-list.css'
@@ -28,14 +38,31 @@ import { LayoutNursingHome } from '../../../shared/presentation/components/layou
 export class ResidentFormList {
   readonly store = inject(ResidentStore);
   protected router = inject(Router);
+
   selectedId: number | null = null;
   imageLoaded = false;
+  searchTerm = signal('');
+  residents = computed(() => this.store.residents());
+  filteredResidents = computed(() => {
+    const term = this.removeAccents(this.searchTerm().toLowerCase().trim());
+    const residents = this.residents();
+
+    if (!term) return residents;
+
+    return residents.filter(r => {
+      const name = this.removeAccents(r.name);
+      const lastname = this.removeAccents(r.lastname);
+      return name.startsWith(term) || lastname.startsWith(term);
+    });
+  });
+
+  removeAccents(word: string) {
+    return word.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, '').trim();
+  }
 
   selectResident(id: number) {
     this.selectedId = this.selectedId === id ? null : id;
   }
-
-  residents = computed(() => this.store.residents());
 
   viewDetails(id: number) {
     this.router.navigate(['resident', id, 'detail']).then();
