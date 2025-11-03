ec1f675b7a7eaab57f2514dc712c057fb14cb4d2
Adrian QC <aqc1019@gmail.com>
2025-11-03 17:18:52 -0500

    feat(staff-member-detail): add staff member search.

--- src/app/staff-management/presentation/staff-member-list/staff-member-list.ts
+++ src/app/staff-management/presentation/staff-member-list/staff-member-list.ts
@@ -1,13 +1,14 @@
-import { Component, computed, inject } from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { MatError } from '@angular/material/form-field';
+import {MatError, MatFormField, MatLabel, MatPrefix, MatSuffix} from '@angular/material/form-field';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { MatButton, MatIconButton } from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
 import { StaffManagementStore } from '../../application/staff-management.store';
 import { LayoutNursingHome } from '../../../shared/presentation/components/layout-nursing-home/layout-nursing-home';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-staff-member-list',
@@ -19,9 +20,14 @@ import { LayoutNursingHome } from '../../../shared/presentation/components/layou
     MatCard,
     MatCardContent,
     MatIcon,
-    MatIconButton,
     MatButton,
-    LayoutNursingHome
+    LayoutNursingHome,
+    MatFormField,
+    MatIconButton,
+    MatInput,
+    MatLabel,
+    MatPrefix,
+    MatSuffix
   ],
   templateUrl: './staff-member-list.html',
   styleUrl: './staff-member-list.css'
@@ -29,13 +35,30 @@ import { LayoutNursingHome } from '../../../shared/presentation/components/layou
 export class StaffMemberList {
   readonly store = inject(StaffManagementStore);
   protected router = inject(Router);
+
   selectedId: number | null = null;
+  searchTerm = signal('');
+  staffMembers = computed(() => this.store.staffMembers());
+  filteredStaffMembers = computed(() => {
+    const term = this.removeAccents(this.searchTerm().toLowerCase().trim());
+    const staffMembers = this.staffMembers();
+
+    if (!term) return staffMembers;
+
+    return staffMembers.filter(r => {
+      const name = this.removeAccents(r.name);
+      const lastname = this.removeAccents(r.lastname);
+      return name.startsWith(term) || lastname.startsWith(term);
+    });
+  });
+
+  removeAccents(text: string) {
+    return text.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, '').trim();
+  }
 
   selectStaff(id: number) {
     this.selectedId = this.selectedId === id ? null : id;
   }
-
-  staff = computed(() => this.store.staffMembers());
 
   viewDetails(id: number) {
     this.router.navigate(['employee', id, 'detail']).then();
