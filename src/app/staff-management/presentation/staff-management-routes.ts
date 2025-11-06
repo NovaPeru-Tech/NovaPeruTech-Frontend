80ad6c025188cbd2e789bc4f3d0655cfce42e36c
Adrian QC <aqc1019@gmail.com>
2025-11-05 19:46:47 -0500

    refactor(staff): update staff routes.

--- src/app/staff-management/presentation/staff-management-routes.ts
+++ src/app/staff-management/presentation/staff-management-routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 
-const staffList=()=>import('./staff-member-list/staff-member-list').then(m=>m.StaffMemberList);
-const staffMemberForm=()=>import('./staff-member-form/staff-member-form').then(m => m.StaffMemberForm);
-const staffDetail=()=> import('./staff-member-detail/staff-member-detail').then(m => m.StaffMemberDetail);
-const StaffManagementRoutes:Routes=[
-  {path:'employee/register',loadComponent:staffMemberForm},
-  {path:'employee/list',loadComponent:staffList},
-  {path:'employee/:id/edit',loadComponent:staffMemberForm},
-  {path:'employee/:id/detail',loadComponent:staffDetail},
-  ]
-export {StaffManagementRoutes }
+// Lazy-loaded components
+const staffMemberList = () =>
+  import('./staff-member-list/staff-member-list').then(m => m.StaffMemberList);
+const staffMemberForm = () =>
+  import('./staff-member-form/staff-member-form').then(m => m.StaffMemberForm);
+const staffMemberDetail = () =>
+  import('./staff-member-detail/staff-member-detail').then(m => m.StaffMemberDetail);
+
+const baseTitle = 'Veyra';
+const staffManagementRoutes:Routes = [
+  { path: 'list',            loadComponent: staffMemberList,   title: `Staff Members | ${baseTitle}` },
+  { path: 'list/:id/detail', loadComponent: staffMemberDetail, title: `Staff Member Detail | ${baseTitle}` },
+  { path: 'list/new',        loadComponent: staffMemberForm,   title: `New Staff Member | ${baseTitle}` },
+  { path: 'list/:id/edit',   loadComponent: staffMemberForm,   title: `Edit Staff Member | ${baseTitle}` }
+];
+
+export { staffManagementRoutes };
