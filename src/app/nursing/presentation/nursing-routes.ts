19dd23f2cf188511d1aabe039634763e94a92e9e
Aqc <aqc1019@gmail.com>
2025-11-08 14:32:32 -0500

    test(nursing-routes): add room views routes.

--- src/app/nursing/presentation/nursing-routes.ts
+++ src/app/nursing/presentation/nursing-routes.ts
@@ -7,6 +7,10 @@ const residentList = () =>
   import('./views/resident-list/resident-list').then(m => m.ResidentList);
 const residentDetail = () =>
   import('./views/resident-detail/resident-detail').then(m => m.ResidentDetail);
+const roomList = () =>
+  import('./views/room-list/room-list').then(m => m.RoomList);
+const roomForm = () =>
+  import('./views/room-form/room-form').then(m => m.RoomForm);
 const nursingHomeRegister = () =>
   import('./views/nursing-home-form/nursing-home-form').then(m => m.NursingHomeForm)
 
@@ -19,9 +23,16 @@ const residentsRoutes: Routes = [
   { path: 'list/:id/edit',   loadComponent: residentForm,   title: `Edit Resident | ${baseTitle}` }
 ];
 
+const roomsRoutes: Routes = [
+  { path: 'list',            loadComponent: roomList,   title: `Rooms | ${baseTitle}` },
+  { path: 'list/new',        loadComponent: roomForm,   title: `New Room | ${baseTitle}` },
+  { path: 'list/:id/edit',   loadComponent: roomForm,   title: `Edit Room | ${baseTitle}` }
+];
+
 const nursingRoutes: Routes = [
   { path:'register/nursingHome', loadComponent:nursingHomeRegister }
 ];
 
 export { residentsRoutes };
+export { roomsRoutes };
 export { nursingRoutes };
