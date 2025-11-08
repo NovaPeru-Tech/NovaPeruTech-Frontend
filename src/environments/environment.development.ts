4ff83416f3debf9e7af3e543d8d0f88b75ffe69f
Adrian QC <aqc1019@gmail.com>
2025-11-08 07:10:55 -0500

    feat(environment): add room endpoint.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,4 +7,5 @@ export const environment = {
   platformProviderResidentPath:'/residents',
   platformProviderEmployeeEndPoint:'/employee',
   platformProviderMedicationEndPoint:'/medications',
+  platformProviderRoomsEndpointPath: '/rooms'
 };
