4af85e3b7c7743fc4484222bdacaa29ac3ad993c
Adrian QC <aqc1019@gmail.com>
2025-11-13 01:33:27 -0500

    feat(hcm): add contracts in hcm api.

--- src/app/hcm/infrastructure/hcm-api.ts
+++ src/app/hcm/infrastructure/hcm-api.ts
@@ -4,16 +4,20 @@ import { BaseApi } from '../../shared/infrastructure/base-api';
 import { HttpClient } from '@angular/common/http';
 import { StaffMember } from '../domain/model/staff-member.entity';
 import { Observable } from 'rxjs';
+import { ContractsApiEndpoint } from './contracts-api-endpoint';
+import { Contract } from '../domain/model/contract.entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HcmApi extends BaseApi {
   private readonly _staffApiEndpoint: StaffApiEndpoint;
+  private readonly _contractsApiEndpoint: ContractsApiEndpoint;
 
   constructor(http:HttpClient) {
     super();
     this._staffApiEndpoint = new StaffApiEndpoint(http);
+    this._contractsApiEndpoint = new ContractsApiEndpoint(http);
   }
 
   createStaffMember(staffMember:StaffMember):Observable<StaffMember> {
@@ -30,5 +34,13 @@ export class HcmApi extends BaseApi {
 
   getStaffMembers() {
     return this._staffApiEndpoint.getAll();
+  }
+
+  createContract(contract: Contract):Observable<Contract> {
+    return this._contractsApiEndpoint.create(contract);
+  }
+
+  getContracts() {
+    return this._contractsApiEndpoint.getAll();
   }
 }
