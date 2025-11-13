ee1f75c4aa18eac0b5bf69433d97f403650e7076
Adrian QC <aqc1019@gmail.com>
2025-11-13 01:35:32 -0500

    feat(hcm): create empty contract form view.

---
+++ src/app/hcm/presentation/views/contract-form/contract-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-form',
+  imports: [],
+  templateUrl: './contract-form.html',
+  styleUrl: './contract-form.css'
+})
+export class ContractForm {
+
+}
