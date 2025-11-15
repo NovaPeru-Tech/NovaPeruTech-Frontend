2ae8d9331d23df4a5a9214220170949d81022654
Aqc <aqc1019@gmail.com>
2025-11-15 17:18:33 -0500

    feat(nursing): add medication list view .

---
+++ src/app/nursing/presentation/views/medication-list/medication-list.ts
@@ -0,0 +1,125 @@
+import { Component, computed, inject, signal, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { NursingStore } from '../../../application/nursing.store';
+import { DatePipe } from '@angular/common';
+import { LayoutNursingHome } from '../../../../shared/presentation/components/layout-nursing-home/layout-nursing-home';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatNoDataRow, MatTable } from '@angular/material/table';
+import { MatError, MatFormField, MatPrefix, MatSuffix } from '@angular/material/form-field';
+import { MatInput, MatLabel } from '@angular/material/input';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-medication-list',
+  imports: [
+    DatePipe,
+    LayoutNursingHome,
+    MatButton,
+    MatCell,
+    MatCellDef,
+    MatColumnDef,
+    MatError,
+    MatFormField,
+    MatHeaderCell,
+    MatIcon,
+    MatIconButton,
+    MatInput,
+    MatLabel,
+    MatPaginator,
+    MatPrefix,
+    MatProgressSpinner,
+    MatSuffix,
+    MatTable,
+    TranslatePipe,
+    MatHeaderCellDef,
+    MatNoDataRow
+  ],
+  templateUrl: './medication-list.html',
+  styleUrl: './medication-list.css'
+})
+export class MedicationList {
+  readonly store = inject(NursingStore);
+  protected router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  displayedColumns: string[] = [
+    'name',
+    'description',
+    'amount',
+    'expirationDate',
+    'drugPresentation',
+    'dosage'
+  ];
+
+  residentId = signal<number | null>(null);
+
+  constructor() {
+    this.route.params.subscribe(params => {
+      const id = params['id'] ? +params['id'] : null;
+      this.residentId.set(id);
+      if (!id) {
+        this.router.navigate(['residents/list']).then();
+        return;
+      }
+    });
+  }
+  searchTerm = signal('');
+  selectedColumn = signal<string>('name');
+  sortDirection = signal<'asc'|'desc'>('asc');
+
+  filteredMedications = computed(() => {
+    const term = this.removeAccents(this.searchTerm().toLowerCase().trim());
+    const meds = this.store.medications().filter(medication => medication.residentId === this.residentId());
+    const col = this.selectedColumn();
+    const dir = this.sortDirection();
+    let result = meds;
+
+    if (term) {
+      result = meds.filter(m => {
+        const name = this.removeAccents(m.name?.toLowerCase() || '');
+        return name.startsWith(term);
+      });
+    }
+
+    return [...result].sort((a, b) => {
+      let valA = (a as any)[col];
+      let valB = (b as any)[col];
+
+      if (valA == null) return 1;
+      if (valB == null) return -1;
+
+      if (typeof valA === 'string') valA = valA.toLowerCase();
+      if (typeof valB === 'string') valB = valB.toLowerCase();
+
+      return dir === 'asc'
+        ? valA > valB ? 1 : -1
+        : valA < valB ? 1 : -1;
+    });
+  });
+
+  removeAccents(word: string) {
+    return word.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, '').trim();
+  }
+
+  toggleSort(col: string) {
+    if (this.selectedColumn() === col) {
+      this.sortDirection.update(d => d === 'asc' ? 'desc' : 'asc');
+    } else {
+      this.selectedColumn.set(col);
+      this.sortDirection.set('asc');
+    }
+  }
+
+  navigateToNew() {
+    this.router.navigate(['inventory/medication/new']).then();
+  }
+
+  getStatusClass(medication: any): string {
+    return medication.getStatus();
+  }
+}
