4b35928fff8c9d09b4470ac1fafad588c4e19d41
Adrian QC <aqc1019@gmail.com>
2025-11-16 12:01:10 -0500

    feat(routes): add medications routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,21 +13,21 @@ const staffRoutes = () =>
   import('./hcm/presentation/hcm-routes').then(m => m.staffRoutes);
 const contractsRoutes = () =>
   import('./hcm/presentation/hcm-routes').then(m => m.contractsRoutes);
-const inventoryRoutes = () =>
-  import('./inventory/presentation/inventory-routes').then(m => m.inventoryRoutes);
+const medicationRoutes = () =>
+  import('./nursing/presentation/nursing-routes').then(m => m.medicationsRoutes);
 const roomRoutes = () =>
   import('./nursing/presentation/nursing-routes').then(m => m.roomsRoutes);
 
 const baseTitle = 'Veyra';
 export const routes: Routes = [
-  { path: 'home',      component: Home,             title:`Home | ${baseTitle}` },
-  { path: 'auth',      loadChildren: iamRoutes },
-  { path: 'nursing',   loadChildren: nursingHomeRoutes },
-  { path: 'residents', loadChildren: residentRoutes },
-  { path: 'staff',     loadChildren: staffRoutes },
-  { path: 'contracts', loadChildren: contractsRoutes },
-  { path: 'inventory', loadChildren: inventoryRoutes },
-  { path: 'rooms',     loadChildren: roomRoutes },
-  { path: '',          redirectTo: '/home',         pathMatch:'full' },
-  { path: '**',        loadComponent: pageNotFound, title:`Page Not Found | ${baseTitle}`}
+  { path: 'home',        component: Home,             title:`Home | ${baseTitle}` },
+  { path: 'auth',        loadChildren: iamRoutes },
+  { path: 'nursing',     loadChildren: nursingHomeRoutes },
+  { path: 'residents',   loadChildren: residentRoutes },
+  { path: 'staff',       loadChildren: staffRoutes },
+  { path: 'contracts',   loadChildren: contractsRoutes },
+  { path: 'medications', loadChildren: medicationRoutes },
+  { path: 'rooms',       loadChildren: roomRoutes },
+  { path: '',            redirectTo: '/home',         pathMatch:'full' },
+  { path: '**',          loadComponent: pageNotFound, title:`Page Not Found | ${baseTitle}`}
 ];
