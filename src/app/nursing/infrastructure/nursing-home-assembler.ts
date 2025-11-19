450faef45a287c5301006053a002a7f1a616c56f
Adrian QC <aqc1019@gmail.com>
2025-11-18 21:58:03 -0500

    refactor(nursing): update nursing home assembler.

--- src/app/nursing/infrastructure/nursing-home-assembler.ts
+++ src/app/nursing/infrastructure/nursing-home-assembler.ts
@@ -1,8 +1,8 @@
 import { BaseAssembler } from '../../shared/infrastructure/base-assembler';
 import { NursingHome } from '../domain/model/nursing-home.entity';
-import { NursingHomesResource, NursingHomesResponse } from './nursing-homes-response';
+import { NursingHomeResource, NursingHomesResponse } from './nursing-homes-response';
 
-export class NursingHomeAssembler implements BaseAssembler<NursingHome, NursingHomesResource, NursingHomesResponse>{
+export class NursingHomeAssembler implements BaseAssembler<NursingHome, NursingHomeResource, NursingHomesResponse>{
   toEntitiesFromResponse(response: NursingHomesResponse): NursingHome[] {
     return response.nursingHome.map(nursingHome=>this.toEntityFromResource(nursingHome));
   }
@@ -11,16 +11,10 @@ export class NursingHomeAssembler implements BaseAssembler<NursingHome, NursingH
 * @purpose: Assembler class to convert between NursingHome entity and NursingHomeResource
 * @description: This class implements methods to transform data from the API resource format to the domain entity format and vice versa.
 * */
-
-  toEntityFromResource(resource: NursingHomesResource): NursingHome {
+  toEntityFromResource(resource: NursingHomeResource): NursingHome {
     return new NursingHome({
       id:resource.id,
-      name:resource.name,
-      ruc:resource.ruc,
-      phoneNumber:resource.phoneNUmber,
-      address:resource.address,
-      description:resource.description,
-      adminId:resource.adminId,
+      businessProfileId: resource.businessProfileId
     });
   }
 
@@ -28,16 +22,10 @@ export class NursingHomeAssembler implements BaseAssembler<NursingHome, NursingH
 * @purpose: Convert NursingHome entity to NursingHomeResource
 * @description: This method takes a NursingHome entity and maps its properties to a NursingHomeResource object suitable for API communication.
 * */
-
-  toResourceFromEntity(entity: NursingHome): NursingHomesResource {
+  toResourceFromEntity(entity: NursingHome): NursingHomeResource {
     return {
       id:entity.id,
-      name:entity.name,
-      ruc:entity.ruc,
-      phoneNUmber:entity.phoneNumber,
-      address:entity.address,
-      description:entity.description,
-      adminId:entity.adminId
-    } as NursingHomesResource;
+      businessProfileId: entity.businessProfileId
+    } as NursingHomeResource;
   }
 }
