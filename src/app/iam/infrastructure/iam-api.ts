d18f0a00e27b107b9766430b64c614a018a977aa
Adrian QC <aqc1019@gmail.com>
2025-11-19 23:29:54 -0500

    feat(iam): implement iam-api with sign-up and sign-in functionality.

--- src/app/iam/infrastructure/iam-api.ts
+++ src/app/iam/infrastructure/iam-api.ts
@@ -1,30 +1,49 @@
 import { Injectable } from '@angular/core';
-import {SignInApiEndpoint} from './sign-in-api-endpoint';
+import {BaseApi} from '../../shared/infrastructure/base-api';
 import {SignUpApiEndpoint} from './sign-up-api-endpoint';
 import {HttpClient} from '@angular/common/http';
-import {BaseApi} from '../../shared/infrastructure/base-api';
+import {SignUpAssembler} from './sign-up-assembler';
+import {SignUpCommand} from '../domain/model/sign-up.command';
 import {Observable} from 'rxjs';
-import {SignIn} from '../domain/model/sign-in.entity';
-import {SignUp} from '../domain/model/sign-up.entity';
+import {SignUpResource} from './sign-up-response';
+import {SignInCommand} from '../domain/model/sign-in.command';
+import {SignInResource} from './sign-in-response';
+import {SignInApiEndpoint} from './sign-in-api-endpoint';
+import {SignInAssembler} from './sign-in-assembler';
 
+/**
+ * API service for identity and access management operations, including sign-up and sign-in.
+ */
+@Injectable({providedIn: 'root'})
+export class IamApi extends BaseApi {
+  private readonly signUpEndpoint: SignUpApiEndpoint;
+  private readonly signInEndpoint: SignInApiEndpoint;
 
-@Injectable({
-  providedIn: 'root'
-})
-export class IamApi extends BaseApi{
-  private readonly _signInApiEndpoint:SignInApiEndpoint;
-  private readonly _signUpApiEndpoint:SignUpApiEndpoint;
-  constructor(http:HttpClient) {
+  /**
+   * Creates a new IamApi instance.
+   * @param http - The HTTP client for making requests.
+   */
+  constructor(http: HttpClient) {
     super();
-    this._signInApiEndpoint=new SignInApiEndpoint(http);
-    this._signUpApiEndpoint=new SignUpApiEndpoint(http);
+    this.signUpEndpoint = new SignUpApiEndpoint(http, new SignUpAssembler());
+    this.signInEndpoint = new SignInApiEndpoint(http, new SignInAssembler());
   }
 
-  createSignIn(sign:SignIn):Observable<SignIn>{
-    return this._signInApiEndpoint.login(sign);
-}
+  /**
+   * Signs up a new user.
+   * @param signUpCommand - The sign-up command containing user credentials.
+   * @returns An observable of the sign-up resource.
+   */
+  signUp(signUpCommand: SignUpCommand): Observable<SignUpResource> {
+    return this.signUpEndpoint.signUp(signUpCommand);
+  }
 
-CreateSignUp(signUp:SignUp){
-return this._signUpApiEndpoint.register(signUp);
-}
+  /**
+   * Signs in a user.
+   * @param signInCommand - The sign-in command containing user credentials.
+   * @returns An observable of the sign-in resource.
+   */
+  signIn(signInCommand: SignInCommand): Observable<SignInResource> {
+    return this.signInEndpoint.signIn(signInCommand);
+  }
 }
