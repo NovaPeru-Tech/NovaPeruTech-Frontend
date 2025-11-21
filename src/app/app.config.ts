e852db3b234159baee354e8199a6d711b2136a0d
Adrian QC <aqc1019@gmail.com>
2025-11-21 14:12:10 -0500

    feat(app): update app config with iam.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,21 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { iamInterceptor } from './iam/infrastructure/iam.interceptor';
 
-import {routes} from './app.routes';
-import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
-import {provideTranslateService} from '@ngx-translate/core';
-import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
-import {authInterceptor} from './shared/interceptors/auth.interceptor';
-
+/**
+ * Application configuration for Angular.
+ * Includes providers for routing, HTTP client, translation, and error handling.
+ */
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([iamInterceptor])),
     provideTranslateService({
       loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
       fallbackLang: 'en'
