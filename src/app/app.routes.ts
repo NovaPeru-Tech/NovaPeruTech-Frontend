2f1c9ff6e12ec75244326c2fd6111f81a9562598
Adrian QC <aqc1019@gmail.com>
2025-11-21 14:10:56 -0500

    feat(routes): update routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,8 @@ import { Home } from './shared/presentation/views/home/home';
 
 const pageNotFound = () =>
   import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
-const signIn = () =>
-  import('./iam/presentation/components/sign-in.component/sign-in.component').then(m => m.SignInComponent);
-const signUp = () =>
-  import('./iam/presentation/components/sign-up.component/sign-up.component').then(m => m.SignUpComponent);
+const iamRoutes = () =>
+  import('./iam/presentation/iam.routes').then(m => m.iamRoutes);
 const nursingHomeRoutes = () =>
   import('./nursing/presentation/nursing-routes').then(m => m.nursingRoutes);
 const residentRoutes = () =>
@@ -22,15 +20,14 @@ const roomRoutes = () =>
 
 const baseTitle = 'Veyra';
 export const routes: Routes = [
-  { path: 'sign-in',   loadComponent:signIn,        title:`Sign In | ${baseTitle}`},
-  { path: 'sign-up',   loadComponent:signUp,        title:`Sign Up | ${baseTitle}`},
-  { path: 'home',      component: Home,             title:`Home | ${baseTitle}` },
-  { path: 'nursing',   loadChildren: nursingHomeRoutes },
-  { path: 'residents', loadChildren: residentRoutes },
-  { path: 'staff',     loadChildren: staffRoutes },
-  { path: 'contracts', loadChildren: contractsRoutes },
+  { path: 'home',        component: Home,             title:`Home | ${baseTitle}` },
+  { path: 'iam',         loadChildren: iamRoutes },
+  { path: 'nursing',     loadChildren: nursingHomeRoutes },
+  { path: 'residents',   loadChildren: residentRoutes },
+  { path: 'staff',       loadChildren: staffRoutes },
+  { path: 'contracts',   loadChildren: contractsRoutes },
   { path: 'medications', loadChildren: medicationRoutes },
-  { path: 'rooms',     loadChildren: roomRoutes },
-  { path: '',          redirectTo: '/home',         pathMatch:'full' },
-  { path: '**',        loadComponent: pageNotFound, title:`Page Not Found | ${baseTitle}`}
+  { path: 'rooms',       loadChildren: roomRoutes },
+  { path: '',            redirectTo: '/home',         pathMatch:'full' },
+  { path: '**',          loadComponent: pageNotFound, title:`Page Not Found | ${baseTitle}`}
 ];
