359d64b95cd97910e7fc353e4d924cbbe4415cb0
Ariana Ramirez <ariana190406@gmail.com>
2025-11-25 01:21:25 -0500

    feat(payment): add accounts-api-endpoint.ts

--- src/app/payments/infrastructure/accounts-api-endpoint.ts
+++ src/app/payments/infrastructure/accounts-api-endpoint.ts
@@ -1,2 +1,5 @@
-export class AccountsApiEndpoint {
-}
+export const AccountsApiEndpoint = {
+  CREATE_ACCOUNT: "/api/payments/accounts",
+  GET_ACCOUNT_BY_ID: (id: string) => `/api/payments/accounts/${id}`,
+  UPDATE_ACCOUNT: (id: string) => `/api/payments/accounts/${id}`,
+};
