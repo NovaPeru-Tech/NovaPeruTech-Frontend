d5547c1da1f11a0de614de62eb71e5a7b0504e8e
Ariana Ramirez <ariana190406@gmail.com>
2025-11-25 02:53:26 -0500

    feat(payment): add subscriptions-response.ts

--- src/app/payments/infrastructure/subscriptions-response.ts
+++ src/app/payments/infrastructure/subscriptions-response.ts
@@ -1,2 +1,9 @@
-export class SubscriptionsResponse {
+export interface SubscriptionResponse {
+  subscriptionId: string;
+  accountId: string;
+  planId: string;
+  cycle: "monthly" | "annual";
+  price: number;
+  status: "active" | "inactive" | "pending";
+  createdAt: string;
 }
