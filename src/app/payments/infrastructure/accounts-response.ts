b058468f2b28f7363e92d5c4b71f1bcb6e18c403
Ariana Ramirez <ariana190406@gmail.com>
2025-11-25 02:52:17 -0500

    feat(payment): add accounts-response.ts

--- src/app/payments/infrastructure/accounts-response.ts
+++ src/app/payments/infrastructure/accounts-response.ts
@@ -1,2 +1,10 @@
-export class AccountsResponse {
+export interface AccountResponse {
+  id: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  country: string;
+  role: "family" | "nursing-home";
+  createdAt: string;
+  updatedAt: string;
 }
