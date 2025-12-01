f02f6032dc3cc750d5a0af70984b6e32693fbe49
Adrian QC <aqc1019@gmail.com>
2025-12-01 04:18:26 -0500

    feat(analytics): simplify metric assembler by removing unused methods

--- src/app/analytics/infrastructure/metric-assembler.ts
+++ src/app/analytics/infrastructure/metric-assembler.ts
@@ -1,11 +1,7 @@
-import { MetricResource, MetricsResponse } from './metrics-response';
+import { MetricResource } from './metrics-response';
 import { Metric } from '../domain/model/metric.entity';
 
 export class MetricAssembler {
-  toEntitiesFromResponse(response: MetricsResponse): Metric[] {
-    return response.metric.map(metric => this.toEntityFromResource(metric));
-  }
-
   toEntityFromResource(resource: MetricResource): Metric {
     return new Metric({
       labels: resource.labels,
@@ -13,14 +9,5 @@ export class MetricAssembler {
       metricType: resource.metricType,
       total: resource.total
     });
-  }
-
-  toResourceFromEntity(entity: Metric): MetricResource {
-    return {
-      labels: entity.labels,
-      values: entity.values,
-      metricType: entity.metricType,
-      total: entity.total
-    } as MetricResource;
   }
 }
