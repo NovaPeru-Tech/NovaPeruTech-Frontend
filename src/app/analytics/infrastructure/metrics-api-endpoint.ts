356149ad3cbfdc6633167e891bdd63569c5c5286
Adrian QC <aqc1019@gmail.com>
2025-12-01 04:18:33 -0500

    feat(analytics): remove unused staff and residents metrics endpoints

--- src/app/analytics/infrastructure/metrics-api-endpoint.ts
+++ src/app/analytics/infrastructure/metrics-api-endpoint.ts
@@ -7,13 +7,8 @@ import { Metric } from '../domain/model/metric.entity';
 import { MetricResource } from './metrics-response';
 
 const analyticsStaffTerminationsEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsStaffTerminationsEndpointPath}`;
-const analyticsStaffTerminationsByMonthEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsStaffTerminationsByMonthEndpointPath}`;
 const analyticsStaffHiresEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsStaffHiresEndpointPath}`;
-const analyticsStaffHiresByMonthEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsStaffHiresByMonthEndpointPath}`;
 const analyticsResidentsAdmissionsEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsResidentsAdmissionsEndpointPath}`;
-const analyticsResidentsAdmissionsByMonthEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsResidentsAdmissionsByMonthEndpointPath}`;
-const analyticsResidentsAdmissionsByDateRangeEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsResidentsAdmissionsByDateRangeEndpointPath}`;
-const analyticsResidentsActiveEndpointUrl = `${environment.platformProviderApiBaseUrl}${environment.platformProviderAnalyticsResidentsActiveEndpointPath}`;
 
 export class MetricsApiEndpoint extends ErrorHandlingEnabledBaseType {
   private readonly assembler = new MetricAssembler();
@@ -46,15 +41,6 @@ export class MetricsApiEndpoint extends ErrorHandlingEnabledBaseType {
     );
   }
 
-  getStaffTerminationsByMonth(nursingHomeId: number, year: number, month: number): Observable<Metric> {
-    const url = analyticsStaffTerminationsByMonthEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
-    const params = { year: year, month: month }
-    return this.http.get<MetricResource>(url, { params }).pipe(
-      map(resource => this.assembler.toEntityFromResource(resource)),
-      catchError(this.handleError(`Failed to fetch data for staff terminations by month for nursing home ID ${nursingHomeId}, year ${year}, and month ${month}`))
-    );
-  }
-
   getStaffHires(nursingHomeId: number, year: number): Observable<Metric> {
     const url = analyticsStaffHiresEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
     const params = {year: year}
@@ -64,48 +50,12 @@ export class MetricsApiEndpoint extends ErrorHandlingEnabledBaseType {
     );
   }
 
-  getStaffHiresByMonth(nursingHomeId: number, year: number, month: number): Observable<Metric> {
-    const url = analyticsStaffHiresByMonthEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
-    const params = {year: year, month: month}
-    return this.http.get<MetricResource>(url, {params}).pipe(
-      map(resource => this.assembler.toEntityFromResource(resource)),
-      catchError(this.handleError(`Failed to fetch data for staff hires by month for nursing home ID ${nursingHomeId}, year ${year}, and month ${month}`))
-    );
-  }
-
   getResidentsAdmissions(nursingHomeId: number, year: number): Observable<Metric> {
     const url = analyticsResidentsAdmissionsEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
     const params = {year: year}
     return this.http.get<MetricResource>(url, {params}).pipe(
       map(resource => this.assembler.toEntityFromResource(resource)),
       catchError(this.handleError(`Failed to fetch data for residents admissions for nursing home ID ${nursingHomeId} and year ${year}`))
-    );
-  }
-
-  getResidentsAdmissionsByMonth(nursingHomeId: number, year: number, month: number): Observable<Metric> {
-    const url = analyticsResidentsAdmissionsByMonthEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
-    const params = {year: year, month: month}
-    return this.http.get<MetricResource>(url, {params}).pipe(
-      map(resource => this.assembler.toEntityFromResource(resource)),
-      catchError(this.handleError(`Failed to fetch data for residents admissions by month for nursing home ID ${nursingHomeId}, year ${year}, and month ${month}`))
-    );
-  }
-
-  getResidentsAdmissionsByDateRange(nursingHomeId: number, startDate: string, endDate: string): Observable<Metric> {
-    const url = analyticsResidentsAdmissionsByDateRangeEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
-    const params = {startDate: startDate, endDate: endDate}
-    return this.http.get<MetricResource>(url, {params}).pipe(
-      map(resource => this.assembler.toEntityFromResource(resource)),
-      catchError(this.handleError(`Failed to fetch data for residents admissions by date range for nursing home ID ${nursingHomeId}, start date ${startDate}, and end date ${endDate}`))
-    );
-  }
-
-  getResidentsActive(nursingHomeId: number, year: number): Observable<Metric> {
-    const url = analyticsResidentsActiveEndpointUrl.replace('{nursingHomeId}', nursingHomeId.toString());
-    const params = {year: year}
-    return this.http.get<MetricResource>(url, {params}).pipe(
-      map(resource => this.assembler.toEntityFromResource(resource)),
-      catchError(this.handleError(`Failed to fetch data for active residents for nursing home ID ${nursingHomeId} and year ${year}`))
     );
   }
 }
