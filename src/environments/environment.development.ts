b896fa16c4a412d3fd5ed4ce146b210b6c9045f2
Aqc <aqc1019@gmail.com>
2025-12-06 14:12:12 -0500

    feat(environment): add endpoint paths.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  platformProviderApiBaseUrl: 'https://backend-nova-peru-tech-hbafacahbreadea5.canadacentral-01.azurewebsites.net/api/v1',
+  platformProviderApiBaseUrl: 'http://localhost:8080/api/v1',
 
   // Analytics Bounded Context
   platformProviderAnalyticsStaffTerminationsEndpointPath: '/nursing-homes/{nursingHomeId}/analytics/staff-terminations',
@@ -19,6 +19,7 @@ export const environment = {
   platformProviderAdministratorsEndpointPath: '/administrators',
 
   // Nursing Bounded Context
+  platformProviderResidentVitalSigsEndpointPath: '/resident/{residentId}/vital-signs',
   platformProviderDevicesEndpointPath: '/devices',
   platformProviderResidentAllergiesEndpointPath: '/residents/{residentId}/allergies',
   platformProviderAdministratorNursingHomesEndpointPath: '/administrators/{administratorId}/nursing-homes',
