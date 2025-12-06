7dfa455c5de04407b2f780c42d11b43f9b165de4
Racso24k <u20211g192@upc.edu.pe>
2025-12-06 13:04:49 -0500

    feat(interceptor): rename iamInterceptor to authenticationInterceptor and enhance token handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,22 +4,19 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
-import { iamInterceptor } from './iam/infrastructure/iam.interceptor';
+import { authenticationInterceptor } from './iam/infrastructure/iam.interceptor';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
-/**
- * Application configuration for Angular.
- * Includes providers for routing, HTTP client, translation, and error handling.
- */
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([iamInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([authenticationInterceptor])),
     provideTranslateService({
       loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
       fallbackLang: 'en'
-    }), provideCharts(withDefaultRegisterables())
+    }),
+    provideCharts(withDefaultRegisterables())
   ]
 };
--- src/app/iam/infrastructure/iam.interceptor.ts
+++ src/app/iam/infrastructure/iam.interceptor.ts
@@ -1,18 +1,18 @@
-import {HttpInterceptorFn} from '@angular/common/http';
-import {inject} from '@angular/core';
-import {IamStore} from '../application/iam.store';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { IamStore } from '../application/iam.store';
 
-export const iamInterceptor: HttpInterceptorFn = (
-  request,
-  next) => {
-  const store = inject(IamStore);
-  // Get the token from local storage.
-  const token = store.currentToken();
-  // If the token exists, add it to the request headers. Otherwise, send the request as is.
-  const handledRequest = token
-    ? request.clone({headers: request.headers.set('Authorization', `Bearer ${token}`)})
-    : request;
-  console.log(token);
-  // Return the handled request.
-  return next(handledRequest);
+export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
+  const iamStore = inject(IamStore);
+
+  const token = iamStore.currentToken();
+
+  if (token && token.length > 0) {
+    const clonedRequest = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    return next(clonedRequest);
+  }
+
+  return next(req);
 };
